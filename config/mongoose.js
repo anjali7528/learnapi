dd541b92d1660dc2a75bbc7aff54e041893b77ab
anjali7528 <anjaligandhi248@gmail.com>
2022-06-19 13:00:00 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,22 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb+srv://admin:admin@cluster0.a7irpfy.mongodb.net/?retryWrites=true&w=majority');
+
+// const db=mongoose.connection;
+
+// db.on('error', console.error.bind(console,"Error connecting to db"));
+
+// db.once('open', function(){
+//     console.log('Connected to db');
+// });
+
+// module.exports = db;
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.a7irpfy.mongodb.net/?retryWrites=true&w=majority');
+main().catch(err => console.log(err));
 
-const db=mongoose.connection;
-
-db.on('error', console.error.bind(console,"Error connecting to db"));
-
-db.once('open', function(){
-    console.log('Connected to db');
-});
-
-module.exports = db;
+async function main() {
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.a7irpfy.mongodb.net/?retryWrites=true&w=majority');
+  console.log("db connected")
+}
